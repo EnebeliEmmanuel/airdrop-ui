5270fd6322d010077451237680ff710cec662045
Elevatedpeace <Peace08182443958>
2022-04-04 20:43:36 +0100

    cool

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,29 +1,29 @@
-import React from 'react';
-import '../App.css';
-import { Button } from './Button';
-import './HeroSection.css';
+import React from "react";
+import "../App.css";
+import { Button } from "./Button";
+import "./HeroSection.css";
 
 function HeroSection() {
   return (
-    <div className='hero-container'>
-      <video src='/videos/video-1.mp4' autoPlay loop muted />
+    <div className="hero-container">
+      <video src="/videos/video-1.mp4" autoPlay loop muted />
       <h1>Blockbuster Movies Awaits</h1>
-      <p>What are you waiting for?</p>
-      <div className='hero-btns'>
+      <p>Feed your eyes</p>
+      <div className="hero-btns">
         <Button
-          className='btns'
-          buttonStyle='btn--outline'
-          buttonSize='btn--large'
+          className="btns"
+          buttonStyle="btn--outline"
+          buttonSize="btn--large"
         >
           GET STARTED and Buy your movie tickets
         </Button>
         <Button
-          className='btns'
-          buttonStyle='btn--primary'
-          buttonSize='btn--large'
-          onClick={console.log('hey')}
+          className="btns"
+          buttonStyle="btn--primary"
+          buttonSize="btn--large"
+          onClick={console.log("hey")}
         >
-          VIEW TICKECT <i className='far fa-play-circle' />
+          VIEW TICKECT <i className="far fa-play-circle" />
         </Button>
       </div>
     </div>
